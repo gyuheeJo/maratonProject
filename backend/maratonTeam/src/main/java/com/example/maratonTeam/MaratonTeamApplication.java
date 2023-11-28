9ec5a755c91d3769e5436ca56d3dcebef3e8efa5
gyuhee <davidgyuhee@gmail.com>
2023-11-27 20:54:27 -0500

    cors modified, application-pdn properties modified

--- backend/maratonTeam/src/main/java/com/example/maratonTeam/MaratonTeamApplication.java
+++ backend/maratonTeam/src/main/java/com/example/maratonTeam/MaratonTeamApplication.java
@@ -1,5 +1,6 @@
 package com.example.maratonTeam;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,9 +17,11 @@ public class MaratonTeamApplication {
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
+			@Value("${appName.allowedApi}")
+			private String AllowedApi;
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:5173")
+				registry.addMapping("/**").allowedOrigins(AllowedApi)
 						.allowedMethods("*").allowCredentials(true).allowedHeaders("*");
 			}
 		};
